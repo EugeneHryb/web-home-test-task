b3a96760abb89c5f7be42fdae9d3f185785e288d
Eugene Hryb <hrubeugene@gmail.com>
2021-10-25 03:27:55 +0300

    Updates

--- src/API/API.js
+++
@@ -1,6 +0,0 @@
-const API = {
-    firstPage: 'https://jordan.ashton.fashion/api/goods/30/comments',
-    sendComments: 'https://jordan.ashton.fashion/api/goods/30/comments',
-}
-
-export default API
